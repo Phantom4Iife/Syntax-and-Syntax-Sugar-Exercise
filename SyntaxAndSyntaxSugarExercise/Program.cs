de526ec9eb625accc8c357b681ee7c534862d8b1
Jeremiah Augustin <Phantom4iife7@gmail.com>
2025-03-02 23:47:13 -0500

    Finished Project

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //I can't tell if you wanted it to look similar or different from the presented teacher's code 
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
         }
     }
 }
